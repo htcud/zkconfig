b38c3e989535067104f74de55206e77cba1f95d6
zhangqidong <zhangqidong@xiaomi.com>
2019-08-08 22:20:43 +0800

    first

---
+++ src/main/java/com/xt/mi/zkconfig/ZkconfigApplication.java
@@ -0,0 +1,13 @@
+package com.xt.mi.zkconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkconfigApplication.class, args);
+    }
+
+}
